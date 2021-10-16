c061e257dca4cd2670de308856dd9aaf07eda6de
Nazarko12 <nazartsiupryk@gmail.com>
2021-10-16 13:15:33 +0200

    Developed an application on the theme of the hardware store

---
+++ android/app/src/main/kotlin/com/example/layout_laboratory/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.layout_laboratory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hardware_store_application/main.dart';
+import 'package:layout_laboratory/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
